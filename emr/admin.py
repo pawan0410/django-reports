be63e40858c999564c2b57a3cc99958c17e1e1c4
Atul Mishra <atul.mishra@aigbusiness.in>
2017-05-31 15:27:24 +0530

    Make appointments section

--- emr/admin.py
+++ emr/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Status
 from .models import StatusGroup
+from .models import AppointmentType
+from .models import AppointmentTypeGroup
+from .models import ResourceUtilizationSlots
 
 
 class AdminStatus(admin.ModelAdmin):
@@ -9,6 +12,15 @@ class AdminStatus(admin.ModelAdmin):
     def has_add_permission(self, request):
         return
 
+
+class AdminAppointmentType(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return
+
 admin.site.register(Status, AdminStatus)
 admin.site.register(StatusGroup)
+admin.site.register(AppointmentType, AdminAppointmentType)
+admin.site.register(AppointmentTypeGroup)
+
+admin.site.register(ResourceUtilizationSlots)
 
--- emr/models.py
+++ emr/models.py
@@ -25,3 +25,36 @@ class StatusGroup(models.Model):
         ordering = ('name',)
 
 
+class AppointmentType(models.Model):
+    original_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class AppointmentTypeGroup(models.Model):
+    name = models.CharField(max_length=255)
+    appointment_type_id = models.ManyToManyField(AppointmentType)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class ResourceUtilizationSlots(models.Model):
+    name = models.CharField(max_length=255)
+    resource_id = models.IntegerField()
+    monday = models.IntegerField()
+    tuesday = models.IntegerField()
+    wednesday = models.IntegerField()
+    thursday = models.IntegerField()
+    friday = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Resource Utilization Slots'
